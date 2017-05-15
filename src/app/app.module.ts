57e756ec7275cdfcc58daca3d557625b4a3d6e16
Alena Alena <no_money@no_honey.com>
2017-05-15 23:02:19 +0200

    add reservation component template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ReservationsComponent } from './reservations/reservations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservationsComponent
   ],
   imports: [
     BrowserModule,
